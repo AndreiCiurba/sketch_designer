3fa5927c85b5ee01c3bf598ed6c938bfd63681d9
Andrei Ciurba <andrei.ciurba@microfocus.com>
2022-02-11 16:26:17 +0200

    ordered arranged itemx by position (width)

--- sketch.js
+++ sketch.js
@@ -121,19 +121,35 @@ function createUtils() {
       crtLevel = shapes[i].getLevel(levels, h);
       append(shapesPerLevel[crtLevel], shapes[i]);
     }
-    console.log(shapesPerLevel)
     let x_val, y_val;
     for (let k = 0;k < levels; k++){
         let len = shapesPerLevel[k].length;
+        let crtArray = shapesPerLevel[k]
+        //sort array
+        for(var jj = 0; jj < len; jj++){
+         for(var j = 0; j < ( len - jj -1 ); j++){
+           if(crtArray[j].x > crtArray[j+1].x){
+
+             var temp = crtArray[j]
+             crtArray[j] = crtArray[j + 1]
+             crtArray[j+1] = temp
+           }
+         }
+       }
+
+
         for (let i = 0;i < len; i++){
             x_val = (i+ 1) * w/(len+1) - w/16;
             y_val = (k + 1)*h/levels - 3*h/(4*levels);
-            shapesPerLevel[k][i].move(x_val, y_val);
+            crtArray[i].move(x_val, y_val);
         }
     }
     shapesPerLevel = []
   });
 }
+
+
+
 
 const userTypes = [
 	"Individual",
