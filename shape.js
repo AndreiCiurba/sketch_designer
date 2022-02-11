44943ef34c7b79a59ff572e737d9dae0d68eb82a
Andrei Ciurba <andrei.ciurba@microfocus.com>
2022-02-11 16:13:09 +0200

    dsads

---
+++ shape.js
@@ -0,0 +1,72 @@
+// Click and Drag an object
+let size = 50;
+
+class Shape {
+  constructor(x, y, icon, name, country) {
+    this.dragging = false;
+    this.x = x;
+    this.y = y;
+
+    this.offsetX = 0;
+    this.offsetY = 0;
+    this.icon = icon;
+    this.name = name;
+    this.country = country;
+    this.div = createDiv(
+      "<i class=\"shape " + icon + "\"></i>" +
+      "<div class=\"shape name\">" + name + "</div>" +
+      "<div class=\"shape country\">" + country + "</div>")
+    this.div.class('instance')
+  }
+  update() {
+    // Adjust location if being dragged
+    if (this.dragging) {
+      this.x = mouseX + this.offsetX;
+      this.y = mouseY + this.offsetY;
+    }
+  }
+
+  show() {
+    stroke(0);
+    // Different fill based on state
+
+    // this.div.updateDiv(this.icon, this.name, this.country)
+    this.div.position(this.x, this.y);
+  }
+
+  pressed() {
+    // Did I click on the rectangle?
+    if (mouseX > this.x && mouseX < this.x + size  && mouseY > this.y && mouseY < this.y + size) {
+      this.dragging = true;
+      // If so, keep track of relative location of click to corner of rectangle
+      this.offsetX = this.x - mouseX;
+      this.offsetY = this.y - mouseY;
+    }
+  }
+
+
+  hide(){
+    this.x = -100;
+    this.y = -100;
+  }
+  move(x_val, y_val){
+    this.x = x_val;
+    this.y = y_val;
+  }
+
+  getLevel(levels, height) {
+    let i;
+    for (i = 0;i < levels; i++){
+      if (this.y < (i + 1)*h/levels){
+        break;
+      }
+    }
+    console.log(i)
+    return i
+  }
+
+  released() {
+    // Quit dragging
+    this.dragging = false;
+  }
+}
